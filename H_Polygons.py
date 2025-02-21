9d9bdc548417877d0fa976ec0d884bcbdd06661c
iamsamienda <samuelendale7373@gmail.com>
2025-02-21 17:47:45 +0300

    add H_Polygons.py for implementing a solution to validate polygon sides based on mood input

---
+++ H_Polygons.py
@@ -0,0 +1,17 @@
+n = int(input())
+mood = list(map(int, input().split()))
+good = mood.count(1)
+
+def valid(sides):
+    curr = sides
+    div = n//sides
+    while curr > 0:
+        for i 
+    
+
+for i in range(n, 2, -1):
+    if good < i or (n-i)%i:
+        continue
+    i
+    
+    
