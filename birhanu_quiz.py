acf384d6f1c6bc93f7b31057d4e83b84dfa1ccf0
iamsamienda <samuelendale7373@gmail.com>
2025-05-07 12:59:07 +0300

    add birhanu_quiz.py for implementing a solution to generate a pattern based on user input

---
+++ birhanu_quiz.py
@@ -0,0 +1,15 @@
+
+# for i in range(0, 10, 2):
+#     print(i)
+
+n = int(input())
+
+for i in range(1, n):
+    print((n - 1 - i) * 's' + "*B" * i + (n - 1 - i) * 's')
+
+
+# numbers = [0, 1, 2, 3, 4]
+
+# #       |
+# for number in numbers:
+#     continue
